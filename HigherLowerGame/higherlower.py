7c24056ef1a115073092653664a1917410c3807b
jyothitamarana <jyothitamarana2001@gmail.com>
2026-05-18 13:35:43 +0530

    Create higherlower.py

---
+++ HigherLowerGame/higherlower.py
@@ -0,0 +1,53 @@
+from art import logo,vs
+from game_data import data
+import random
+from replit import clear
+
+def format_data(account):
+    account_name=account["name"]
+    account_desc=account["description"]
+    account_country=account["country"]
+    return f"{account_name}, a{account_desc}, from {account_country}"
+def check_answer(guess,a_followers,b_followers):
+    if a_followers > b_followers:
+        return guess=="a"
+    else:
+        return guess=="b"
+
+#Display art
+print(logo)
+score=0
+game_should_continue=True
+
+#Generate a random account from game data
+account_b=random.choice(data)
+
+#Make the game repeated:
+while game_should_continue:
+    account_a=account_b
+    account_b=random.choice(data)
+    if account_a == account_b:
+        account_b=random.choice(data)
+    print(f"Compare A: {format_data(account_a)}")
+    print(vs)
+    print(f"Against B: {format_data(account_b)}")
+
+#Ask user a guess
+    guess=input("Who has more followers? Type 'A' or 'B': ").lower()
+
+#guess follower count of each account
+    a_follower_count=account_a["follower_count"]
+    b_follower_count=account_b["follower_count"]
+    is_correct=check_answer(guess,a_follower_count,b_follower_count)
+
+#clear the screen between rounds
+    clear()
+
+    #Give user feedback on their guess
+    #score keeping
+    if is_correct:
+        score+=1
+        print(f"You're right! current score: {score}")
+    else:
+        game_should_continue=False
+        print(f"Sorry,That's wrong! Final score: {score}")
